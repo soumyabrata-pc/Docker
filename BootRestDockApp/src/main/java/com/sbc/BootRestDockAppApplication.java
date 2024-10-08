bdac63f97c2a34b3c8f131cd348ccdc4496bb93d
Soumyabrata-pc <thesoumyabratac@gmail.com>
2024-10-09 02:19:06 +0530

    bootrest commit

---
+++ BootRestDockApp/src/main/java/com/sbc/BootRestDockAppApplication.java
@@ -0,0 +1,13 @@
+package com.sbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestDockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestDockAppApplication.class, args);
+	}
+
+}
